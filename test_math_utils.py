8e566f5c20d756f2360b15214b73fe7e0c4f9c4b
Test User <test@example.com>
2026-08-13 17:43:43 +0530

    Reintroduce failing test for PR creation workflow test

--- test_math_utils.py
+++ test_math_utils.py
@@ -2,5 +2,5 @@ from math_utils import add
 
 
 def test_add():
-    # Deliberately failing assertion for self-hosted GitHub Actions workflow testing
-    assert add(50, 50) == 999
+    # Deliberately failing assertion for GitHub Actions PR creation testing
+    assert add(100, 200) == 9999

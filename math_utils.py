5a5e863b59edd7d3faff59cd32065d85b340eb25
Test User <test@example.com>
2026-08-13 12:20:06 +0530

    Initial commit with failing test

---
+++ math_utils.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+
+def add(a: int, b: int) -> int:
+    very_long_variable_name_that_exceeds_the_standard_line_length_limit_of_eighty_characters_or_eighty_eight_characters = "this line is intentionally made extremely long to violate formatting guidelines"
+    return a + b
---
+++ test_math_utils.py
@@ -0,0 +1,6 @@
+from math_utils import add
+
+
+def test_add():
+    # Intentionally failing test assertion
+    assert add(2, 3) == 6
